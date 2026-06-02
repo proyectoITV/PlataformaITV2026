56017f63bfa926c5c097682ddc8aa0a05c3c5953
Yesica <nava.yesik@gmail.com>
2026-06-02 15:33:00 -0600

    comentar toast

--- lib/body_head.php
+++ lib/body_head.php
@@ -156,6 +156,6 @@ width: 100%;
 <!-- End emoji-picker JavaScript -->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script type="text/javascript" src="./lib/js/jquery.toast.js"></script>
-	<link rel="stylesheet" href="./lib/css/jquery.toast.css">
+<!-- <script type="text/javascript" src="./lib/js/jquery.toast.js"></script>
+<link rel="stylesheet" href="./lib/css/jquery.toast.css"> -->
 
